23db86c555c66803f877c10af0a47285edaca5b4
chenlong <502143496@qq.com>
2017-07-08 09:46:05 +0800

    view-scroll学习更新

---
+++ data.js
@@ -0,0 +1,44 @@
+var data = {
+    activity: [
+        {
+            activityID: 1,
+            activityTitle: '我是第一个活动标题',
+            activityInt: '我是第一个活动的活动介绍，这个活动非常好玩，大家快来参加！',
+            activityDate: '2017年6月23日',
+            onlineTime: '2017年5月31日',
+            duration: '1小时',
+            activityImage: [
+                '/images/swiper/1.jpg',
+                '/images/swiper/2.jpg',
+                '/images/swiper/3.jpg',
+                ]
+        },
+        {
+            activityID: 2,
+            activityTitle: '我是第二个活动标题',
+            activityInt: '我是第二个活动的活动介绍，这个活动非常好玩，大家快来参加！',
+            activityDate: '2017年6月23日',
+            onlineTime: '2017年5月31日',
+            duration: '1小时',
+            activityImage: [
+                '/images/swiper/1.jpg',
+                '/images/swiper/2.jpg',
+                '/images/swiper/3.jpg',
+            ]
+        },
+        {
+            activityID: 3,
+            activityTitle: '我是第三个活动标题',
+            activityInt: '我是第三个活动的活动介绍，这个活动非常好玩，大家快来参加！',
+            activityDate: '2017年6月23日',
+            onlineTime: '2017年5月31日',
+            duration: '1小时',
+            activityImage: [
+                '/images/swiper/1.jpg',
+                '/images/swiper/2.jpg',
+                '/images/swiper/3.jpg',
+            ]
+        }
+    ]
+};
+module.exports.data = data;
--- template/activity_item/activity_item.js
+++ template/activity_item/activity_item.js
@@ -1,39 +1,32 @@
-var data = require('../../data.js');
-// pages/index/index.js
+// template/activity_item/activity_item.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-      page_num: 1,
-      activity_data: data.data.activity.slice(0, 5),
-      swiperUrls: [
-          '/images/swiper/1.jpg',
-          '/images/swiper/2.jpg',
-          '/images/swiper/3.jpg',
-      ],
-      tag: ['学术活动','社团活动','外语讲座','人工智能','学院通知','报销流程'],
-      temp_data: [{'data':1},{'data':2}]
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-      
+  
   },
 
   /**
@@ -69,8 +62,5 @@ Page({
    */
   onShareAppMessage: function () {
   
-  },
-  bindscroll: function(e){
-      console.log('e.detail.scrollTop');
   }
 })
