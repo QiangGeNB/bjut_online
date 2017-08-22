9ac14d6e3a4a4f31f5051c88e2feac3af34cc2e0
liyan <609505217@qq.com>
2017-08-22 16:52:04 +0800

    认证页面学院去掉全校

--- app.js
+++ app.js
@@ -104,8 +104,8 @@ App({
     },
     globalData: {
         userInfo: null,
-        ServerUrl: 'https://www.i-exshare.cn',
-        //ServerUrl:'http://127.0.0.1:3000',
+        //ServerUrl: 'https://www.i-exshare.cn',
+        ServerUrl:'http://127.0.0.1:3000',
         academy: [
           {
             "_id": "598b0494fb89956113a5f19f",
