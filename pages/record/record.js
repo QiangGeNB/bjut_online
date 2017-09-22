1cb49439913936cf0e685542f90a9a97152720ea
ChenLong <502143496@qq.com>
2017-09-22 16:37:08 +0800

    添加随动背景

--- pages/record/record.js
+++ pages/record/record.js
@@ -5,62 +5,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    frist_tap_x: 0,
+    frist_tap_y: 0,
+    show_x: 0,
+    show_y: 0,
+    touch_mark: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
+  },
+  //记录开始的坐标
+  handletouchstart: function (event) {
+    let start_x = event.touches[0].pageX;
+    let start_y = event.touches[0].pageY;
+    this.setData({
+      frist_tap_x: start_x,
+      frist_tap_y: start_y,
+      touch_mark: true
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  handletouchmove: function(event){
+    let now_x = event.touches[0].pageX;
+    let now_y = event.touches[0].pageY;
+
+    let gap_x = this.data.show_x - (this.data.frist_tap_x - now_x)/200;
+    let gap_y = this.data.show_y - (this.data.frist_tap_y - now_y)/200;
+    
+    this.setData({
+      show_x: gap_x,
+      show_y: gap_y
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  handletouchend: function (event) {
+    console.log('this is handletouchend');
+    this.setData({
+      touch_mark: false
+    });
   }
 })
