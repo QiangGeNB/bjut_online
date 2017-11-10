80878987e222d7efb9b8701645805bb5c8f7309c
ChenLong <502143496@qq.com>
2017-11-10 18:31:31 +0800

    完善注册界面js fix退出活动页面跳转逻辑 修改记录界面显示数据

--- pages/record2/record2.js
+++ pages/record2/record2.js
@@ -22,8 +22,6 @@ Page({
       bjut_id: wx.getStorageSync('user_key'),
       nDays: 7
     };
-    // 查询近x天的平均步数
-    app.SendRequest('/api/get_wx_avg_rank_by_id', get_avg_rank, self.get_wx_avg_rank_request_suc);
     // 查询冠军信息
     app.SendRequest('/api/find_wx_rundata_by_id', {'bjut_id': wx.getStorageSync('user_key')}, function(res){
       console.log('查询到打卡天数：', res);
@@ -51,8 +49,8 @@ Page({
           best_mark: false
         });
       } else {
+        // 存在最佳记录
         var date_list =[];
-        
         var best_record = res.data.data.ranks;
         date_list.push(best_record.date);
         var best_date = self.date_formate(date_list);
@@ -62,13 +60,15 @@ Page({
           best_percentage: best_record.defeatPercentage,
           best_mark: true
         });
+        // 获取最佳 击败 百分数
+        var defeat_data = res.data.defeatPercentage.ranks;
+        self.setData({
+          defeat_date: self.date_formate([defeat_data.date])[0],
+          defeat_percentage: defeat_data.defeatPercentage
+        });
       }
 
     });
-  },
-  get_wx_avg_rank_request_suc: function (res) {
-    console.log('this is get_wx_avg_rank_request_suc...');
-    console.log(res);
   },
 
   // 格式化时间（年、月、日） 传入参数为数组格式
