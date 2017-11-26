4e3d7bf4ecbaf8ef064f3c29d0fa76ef04673e5a
chenlong <502143496@qq,com>
2017-11-26 22:28:21 +0800

    添加手机号js部分

--- pages/update_info/update_info.js
+++ pages/update_info/update_info.js
@@ -7,6 +7,7 @@ Page({
         user_info: {},
         new_nickName: '',
         switch_statu: '',
+        phone_number: '',
         phone_input_mark: false
     },
 
@@ -32,6 +33,7 @@ Page({
           console.log('用户没有手机号');
         } else {
           console.log('用户有手机号');
+          // 手机号输入框是否disable
           this.setData({
             phone_input_mark: true
           });
@@ -43,13 +45,34 @@ Page({
             phone_number: phone
         });
     },
+    // 点击提交按钮
     click_save: function () {
-        let setting_data = {
-            _id: this.data.user_info._id,
-            nickName: this.data.new_nickName,
-            rank_switch: this.data.switch_statu
+      let _this = this;
+      console.log(this.data.phone_number.length);
+        if(this.data.phone_number.length !=11) {
+          wx.showModal({
+            title: '信息错误',
+            content: '手机号格式不正确',
+          });
+        } else if (this.data.phone_number.length == 11 && !this.data.phone_input_mark) { // 格式正确 手机号进行了修改
+        wx.showModal({
+          title: '提示',
+          content: '手机号一经提交，不得修改，您确认提交此手机号吗？',
+          success: function(res) {
+            // 确认修改手机号
+            if(res.confirm) {
+              let setting_data = {
+                _id: _this.data.user_info._id,
+                nickName: _this.data.new_nickName,
+                rank_switch: _this.data.switch_statu,
+                phone_number: _this.data.phone_number
+              }
+              app.SendRequest('/api/update_student', setting_data, _this.user_setting_success_cb);
+            }
+          }
+        })
+         
         }
-        app.SendRequest('/api/update_student', setting_data, this.user_setting_success_cb);
     },
     user_setting_success_cb: function (res) {
         console.log(res);
@@ -68,4 +91,9 @@ Page({
             new_nickName: e.detail.value
         });
     },
+    phone_number_change: function(e) {
+      this.setData({
+        phone_number: e.detail.value
+      });
+    }
 })
