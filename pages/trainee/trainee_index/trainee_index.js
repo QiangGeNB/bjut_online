5a2d6e9f3b157350ea5732231d621d7410fe24c7
chenlong <502143496@qq,com>
2017-12-07 00:02:53 +0800

    添加实习相关界面 实习具体信息界面明天再写

--- pages/trainee/trainee_index/trainee_index.js
+++ pages/trainee/trainee_index/trainee_index.js
@@ -1,5 +1,5 @@
 // pages/trainee/trainee.js
-var trainData = require('../../trainee.js');
+var trainData = require('../../../trainee.js');
 var app = getApp();
 Page({
 
@@ -14,58 +14,66 @@ Page({
 
 
   onLoad: function (options) {
-  
+
   },
 
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
-  click_function_button: function(){
+  click_function_button: function () {
     console.log('点击了功能按钮...');
     this.setData({
       pop: !this.data.pop
     })
+  },
+  // 点击trainee_item
+  click_item: function (e) {
+    var trainee_id = e.currentTarget.dataset.traineeid;
+    wx.navigateTo({
+      url: '/pages/trainee/trainee_detail/trainee_detail?' + trainee_id,
+    })
+    console.log("点击了实习item", e.currentTarget.dataset.traineeid);
   }
 })
